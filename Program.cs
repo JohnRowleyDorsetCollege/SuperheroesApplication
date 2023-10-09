3a091dc0d8647a25638fb376291f3a767ed087c2
dctutor <dctutor@LAPTOP-PEVUSRRH>
2023-10-09 11:33:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Superheroes.Models;
+
+
+//SuperHero superHero = new SuperHero();
+
+//superHero.Name = "Super hero";
+
+//superHero.Fly();
+
+SuperMan superMan = new SuperMan();
+
+
+superMan.Fly();
